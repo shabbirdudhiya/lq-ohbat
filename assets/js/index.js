adc3d3e2cdfe26a102e9310b8b17c0dbbd4df1ae
Shabbir Dudhiyawala <66951720+Shabbir-Dudhiyawala@users.noreply.github.com>
2022-04-15 14:59:44 +0530

    progress bar

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,48 +4,93 @@ var quizProgress;
 var kitProgress;
 
 function setItems() {
-    if (localStorage.getItem('menApparelProgress') && localStorage.getItem('womenApparelProgress') && localStorage.getItem('quizProgress') && localStorage.getItem('kitProgress')) {
-        return;
-    } else {
-        localStorage.setItem('menApparelProgress', 0)
-        localStorage.setItem('womenApparelProgress', 0)
-        localStorage.setItem('quizProgress', 0)
-        localStorage.setItem('kitProgress', 0)
-        localStorage.setItem('isItsSaved', false)
-    }
+  if (
+    localStorage.getItem("menApparelProgress") &&
+    localStorage.getItem("womenApparelProgress") &&
+    localStorage.getItem("quizProgress") &&
+    localStorage.getItem("kitProgress")
+  ) {
+    return;
+  } else {
+    localStorage.setItem("menApparelProgress", 0);
+    localStorage.setItem("womenApparelProgress", 0);
+    localStorage.setItem("quizProgress", 0);
+    localStorage.setItem("kitProgress", 0);
+    localStorage.setItem("isItsSaved", false);
+  }
 }
 function getItems() {
-    menApperalProgress = localStorage.getItem('menApparelProgress')
-    womenApperalProgress = localStorage.getItem('womenApparelProgress')
-    quizProgress = localStorage.getItem('quizProgress')
-    kitProgress = localStorage.getItem('kitProgress')
+  menApperalProgress = localStorage.getItem("menApparelProgress");
+  womenApperalProgress = localStorage.getItem("womenApparelProgress");
+  quizProgress = localStorage.getItem("quizProgress");
+  kitProgress = localStorage.getItem("kitProgress");
 }
 
 function setValues() {
-    let menApperalProgressBar = $('#men-apperal-progress-bar');
-    let womenApperalProgressBar = $('#women-apperal-progress-bar');
-    let quizProgressBar = $('#quiz-progress-bar');
-    let kitProgressBar = $('#kit-progress-bar');
+  let menApperalProgressBar = $("#men-apperal-progress-bar");
+  let womenApperalProgressBar = $("#women-apperal-progress-bar");
+  let quizProgressBar = $("#quiz-progress-bar");
+  let kitProgressBar = $("#kit-progress-bar");
 
-    // Set Percentage
-    percentageMenApperalProgress = parseInt(((menApperalProgress / 100) * 100), 10);
-    percentageWomenApperalProgressBar = parseInt(((womenApperalProgress / 100) * 100), 10);
-    percentageQuizProgressBar = parseInt(((quizProgress / 100) * 100), 10);
-    percentageKitProgressBar = parseInt(((kitProgress / 100) * 100), 10);
-    // Set html
+  // Set Percentage
+  percentageMenApperalProgress = parseInt((menApperalProgress / 100) * 100, 10);
+  percentageWomenApperalProgressBar = parseInt(
+    (womenApperalProgress / 100) * 100,
+    10
+  );
+  percentageQuizProgressBar = parseInt((quizProgress / 100) * 100, 10);
+  percentageKitProgressBar = parseInt((kitProgress / 100) * 100, 10);
+
+  if (percentageMenApperalProgress == 0) {
+    menApperalProgressBar.text("Men");
+    menApperalProgressBar.css("width", "100" + "%");
+  } else {
     menApperalProgressBar.text(percentageMenApperalProgress + "% -  Men");
-    womenApperalProgressBar.text(percentageWomenApperalProgressBar + "% - Women");
+    menApperalProgressBar.css("width", percentageMenApperalProgress + "%");
+  }
+
+  if (percentageWomenApperalProgressBar == 0) {
+    womenApperalProgressBar.text("Women");
+    womenApperalProgressBar.css("width", "100" + "%");
+  } else {
+    womenApperalProgressBar.text(
+      percentageWomenApperalProgressBar + "% - Women"
+    );
+    womenApperalProgressBar.css(
+      "width",
+      percentageWomenApperalProgressBar + "%"
+    );
+  }
+
+  if (percentageQuizProgressBar == 0) {
+    quizProgressBar.text("Quiz");
+    quizProgressBar.css("width", "100" + "%");
+  } else {
     quizProgressBar.text(percentageQuizProgressBar + "% - Quiz");
+    quizProgressBar.css("width", percentageQuizProgressBar + "%");
+  }
+
+  if (percentageKitProgressBar == 0) {
+    kitProgressBar.text("Kit");
+    kitProgressBar.css("width", "100" + "%");
+  } else {
     kitProgressBar.text(percentageKitProgressBar + "% - Kit");
-    
-    // Set Progress bar
-    menApperalProgressBar.css("width",percentageMenApperalProgress + "%");
-    womenApperalProgressBar.css("width",percentageWomenApperalProgressBar + "%");
-    quizProgressBar.css("width",percentageQuizProgressBar + "%");
-    kitProgressBar.css("width",percentageKitProgressBar + "%");
+    kitProgressBar.css("width", percentageKitProgressBar + "%");
+  }
+
+  // Set html
+  // menApperalProgressBar.text(percentageMenApperalProgress + "% -  Men");
+  // womenApperalProgressBar.text(percentageWomenApperalProgressBar + "% - Women");
+  // quizProgressBar.text(percentageQuizProgressBar + "% - Quiz");
+  //   kitProgressBar.text(percentageKitProgressBar + "% - Kit");
+
+  // Set Progress bar
+  // menApperalProgressBar.css("width",percentageMenApperalProgress + "%");
+  // womenApperalProgressBar.css("width",percentageWomenApperalProgressBar + "%");
+  // quizProgressBar.css("width",percentageQuizProgressBar + "%");
+  //   kitProgressBar.css("width", percentageKitProgressBar + "%");
 }
 
 setItems();
 getItems();
 setValues();
-
