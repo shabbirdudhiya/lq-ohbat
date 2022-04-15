e5a73ed473c55a3ed3f267c7a29053d2e17e1564
Shabbir Dudhiyawala <66951720+Shabbir-Dudhiyawala@users.noreply.github.com>
2022-04-16 00:24:37 +0530

    Stepper

---
+++ assets/js/stepper.js
@@ -0,0 +1,20 @@
+if (
+  localStorage.getItem("menApparelProgress") == 100 &&
+  localStorage.getItem("womenApparelProgress") == 100
+) {
+  $(".step1").removeClass("step-active");
+  $(".step1").addClass("step-completed");
+  $(".step2").addClass("step-active");
+  //   Step Two
+  if (localStorage.getItem("quizProgress") == 100) {
+    $(".step2").removeClass("step-active");
+    $(".step2").addClass("step-completed");
+    $(".step3").addClass("step-active");
+  }
+  //   Step Three
+  if (localStorage.getItem("kitProgress") == 100) {
+    $(".step3").removeClass("step-active");
+    $(".step3").addClass("step-completed");
+    $(".step4").addClass("step-active");
+  }
+}
