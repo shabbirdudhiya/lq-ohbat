f85de3386a7f8fce01d0573b8e6d35d88ee0b445
Shabbir Dudhiyawala <66951720+Shabbir-Dudhiyawala@users.noreply.github.com>
2022-04-15 15:43:54 +0530

    Update toggle-full-screen.js

--- assets/js/toggle-full-screen.js
+++ assets/js/toggle-full-screen.js
@@ -1,13 +1,44 @@
-
-$('.toggle-full-screen').click(function () {
-    toggleFullScreen(); 
+$(".toggle-full-screen").click(function () {
+  toggleFullScreen();
 });
+
 function toggleFullScreen() {
-    if (!document.fullscreenElement) {
-        document.documentElement.requestFullscreen();
-    } else {
-        if (document.exitFullscreen) {
-            document.exitFullscreen();
-        }
+  //   if (document.documentElement.requestFullScreen) {
+  //     document.documentElement.requestFullScreen();
+  //   } else if (document.documentElement.mozRequestFullScreen) {
+  //     document.documentElement.mozRequestFullScreen();
+  //   } else if (document.documentElement.webkitRequestFullScreen) {
+  //     document.documentElement.webkitRequestFullScreen(
+  //       Element.ALLOW_KEYBOARD_INPUT
+  //     );
+  //   }
+
+  elem = document.documentElement;
+
+  if (
+    !document.fullscreenElement &&
+    !document.mozFullScreenElement &&
+    !document.webkitFullscreenElement &&
+    !document.msFullscreenElement
+  ) {
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+    } else if (elem.msRequestFullscreen) {
+      elem.msRequestFullscreen();
+    } else if (elem.mozRequestFullScreen) {
+      elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) {
+      elem.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
     }
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.msExitFullscreen) {
+      document.msExitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    }
+  }
 }
