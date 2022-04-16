677043d33d47e318327a87d233a83f18e750cd17
Shabbir Dudhiyawala <66951720+Shabbir-Dudhiyawala@users.noreply.github.com>
2022-04-16 15:10:52 +0530

    analytics

---
+++ assets/js/gtag.js
@@ -0,0 +1,9 @@
+// <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-MJDM46174J"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-MJDM46174J');
+</script>
