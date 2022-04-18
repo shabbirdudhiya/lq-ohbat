85e99a9c54dcc11a81ed230c6d8c19033698f02f
Shabbir Dudhiyawala <66951720+Shabbir-Dudhiyawala@users.noreply.github.com>
2022-04-18 22:09:33 +0530

    .

--- assets/js/gtag.js
+++ assets/js/gtag.js
@@ -1,7 +1,8 @@
-<script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag() {dataLayer.push(arguments)}; }
-    gtag('js', new Date());
-    gtag('config', 'G-MJDM46174J');
-</script>
+ <script async src="https://www.googletagmanager.com/gtag/js?id=G-MJDM46174J"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag() { dataLayer.push(arguments); }
+        gtag('js', new Date());
+        gtag('config', 'G-MJDM46174J');
+    </script>
    
