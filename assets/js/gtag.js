86a9f00e3843953a9eafbdf1fd98f759f197500f
Shabbir Dudhiyawala <66951720+Shabbir-Dudhiyawala@users.noreply.github.com>
2022-04-18 14:51:27 +0530

    analytics

--- assets/js/gtag.js
+++ assets/js/gtag.js
@@ -1,9 +1,8 @@
-// <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-MJDM46174J"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-MJDM46174J');
-</script>
+ <script async src="https://www.googletagmanager.com/gtag/js?id=G-MJDM46174J"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag() { dataLayer.push(arguments); }
+        gtag('js', new Date());
+        gtag('config', 'G-MJDM46174J');
+    </script>
+   
