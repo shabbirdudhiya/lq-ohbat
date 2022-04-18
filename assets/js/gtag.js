f58477a3f89c42db3352bee392db9cc44821d967
Shabbir Dudhiyawala <66951720+Shabbir-Dudhiyawala@users.noreply.github.com>
2022-04-18 22:07:30 +0530

    videos || analytics

--- assets/js/gtag.js
+++ assets/js/gtag.js
@@ -1,8 +1,7 @@
- <script async src="https://www.googletagmanager.com/gtag/js?id=G-MJDM46174J"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag() { dataLayer.push(arguments); }
-        gtag('js', new Date());
-        gtag('config', 'G-MJDM46174J');
-    </script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {dataLayer.push(arguments)}; }
+    gtag('js', new Date());
+    gtag('config', 'G-MJDM46174J');
+</script>
    
