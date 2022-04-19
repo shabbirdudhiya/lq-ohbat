b66524d4b0e0022d7ed5f90b33d2dc3f8e5b1435
Shabbir Dudhiyawala <66951720+Shabbir-Dudhiyawala@users.noreply.github.com>
2022-04-19 15:31:59 +0530

    Names changes

--- assets/js/gtag.js
+++ assets/js/gtag.js
@@ -1,8 +1,9 @@
- <script async src="https://www.googletagmanager.com/gtag/js?id=G-MJDM46174J"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag() { dataLayer.push(arguments); }
-        gtag('js', new Date());
-        gtag('config', 'G-MJDM46174J');
-    </script>
-   
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-G3CVBEPHBE"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-G3CVBEPHBE');
+</script>
